e09e09b5acea9eb949a651739bf6758057131004
m4tta <matt@fizzled.me>
2016-07-15 16:32:58 -0700

    git

---
+++ README.md
@@ -0,0 +1,6 @@
+# csv-to-ngraph
+
+##### Usage
+    $ git clone https://github.com/m4tta/csv-to-ngraph.git
+    $ npm install
+    $ npm start -- -i filename.csv
---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const argv = require('yargs').argv;
+const ProgressBar = require('progress');
+const graph = require('ngraph.graph')();
+const createLayout = require('ngraph.offline.layout');
+const toBinary = require('ngraph.tobinary');
+const file = fs.readFileSync(argv.i, 'utf-8');
+const data = file.replace(/ /g, '').split('\r\n');
+
+let current = 0;
+var total = data.length;
+
+console.log(`${total} total items in file`);
+console.log(`beginning processing:`);
+
+let bar = new ProgressBar(':bar :percent[:current/:total] ETA: :etas (:elapsed)', {
+    total,
+    width: 60,
+    complete: '█',
+    incomplete: '░'
+});
+data.forEach(function (line) {
+    const [src, dst] = line.split('|');
+    graph.addLink(src, dst);
+    bar.tick();
+});
+
+console.log('done');
+
+const layout = createLayout(graph, {
+    layout: require('ngraph.forcelayout3d')
+});
+
+//layout.run();
+toBinary(graph);
